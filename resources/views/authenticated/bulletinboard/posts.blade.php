5635550b4725392fecebdaa45ead08950a1ef65b
横山翼 <>
2025-01-12 14:06:44 +0900

    掲示板レイアウト完了

--- resources/views/authenticated/bulletinboard/posts.blade.php
+++ resources/views/authenticated/bulletinboard/posts.blade.php
@@ -1,36 +1,36 @@
 <x-sidebar>
 <div class="board_area w-100 border m-auto d-flex">
   <div class="post_view w-75 mt-5">
-    <p class="w-75 m-auto">投稿一覧</p>
+    <p class="w-75 m-auto"></p>
     @foreach($posts as $post)
     <div class="post_area border w-75 m-auto p-3">
-      <p>
+      <p class="post-name">
         <span>{{ $post->user->over_name }}</span>
         <span class="ml-3">{{ $post->user->under_name }}</span>さん
       </p>
-      <p><a href="{{ route('post.detail', ['id' => $post->id]) }}">{{ $post->post_title }}</a></p>
+      <p><a href="{{ route('post.detail', ['id' => $post->id]) }}" style="color: black; font-weight: bold;">{{ $post->post_title }}</a></p>
       <div class="post_bottom_area d-flex">
-        <div class="d-flex post_status">
+        <div class="category-box">
+          @if ($post->subCategories->isNotEmpty())
+            @foreach ($post->subCategories as $subCategory)
+              <p>{{ $subCategory->sub_category }}</p>
+            @endforeach
+          @else
+            <p></p>
+          @endif
+        </div>
+        <div class="d-flex post_status" style="color: #999999;">
           <div class="mr-5">
-            <i class="fa fa-comment"></i><span class="">{{ $post->post_comments_count }}</span>
+            <i class="fa fa-comment"></i><span class="" style="color: black;">{{ $post->post_comments_count }}</span>
           </div>
           <div>
             @if(Auth::user()->is_Like($post->id))
-            <p class="m-0"><i class="fas fa-heart un_like_btn" post_id="{{ $post->id }}"></i><span class="like_counts{{ $post->id }}">{{ $post->like_count }}</span></p>
+            <p class="m-0"><i class="fas fa-heart un_like_btn" post_id="{{ $post->id }}"></i><span class="like_counts{{ $post->id }}" style="color: black;">{{ $post->like_count }}</span></p>
             @else
-            <p class="m-0"><i class="fas fa-heart like_btn" post_id="{{ $post->id }}"></i><span class="like_counts{{ $post->id }}">{{ $post->like_count }}</span></p>
+            <p class="m-0"><i class="far fa-heart like_btn" post_id="{{ $post->id }}"></i><span class="like_counts{{ $post->id }}" style="color: black;">{{ $post->like_count }}</span></p>
             @endif
           </div>
         </div>
-      </div>
-      <div class="">
-        @if ($post->subCategories->isNotEmpty())
-          @foreach ($post->subCategories as $subCategory)
-            <p>{{ $subCategory->sub_category }}</p>
-          @endforeach
-        @else
-          <p>サブカテゴリーはありません</p>
-        @endif
       </div>
     </div>
     @endforeach
